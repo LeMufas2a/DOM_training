3919b943fff40cdb06095211315dd09801d1c783
LeMufas2a <codage.mufasa@gmail.com>
2021-02-19 22:27:57 +0100

    Exo_01

---
+++ recuperation_elements_part1.1/main.js
@@ -0,0 +1,22 @@
+// ### 1. Stock tous les titres h1 dans une variable et affiche le contenu de cette variable dans un console.log 
+
+/* let h1 = document.getElementsByTagName('h1')
+console.log(h1); */
+
+//2. Après avoir récupéré tous les h3, stock chaque sous titre h3 dans des variables différentes et affiches leur contenu dans un console.log différent
+
+/* let h3 = document.getElementsByTagName('h3')
+console.log(h3[0]);
+console.log(h3[1]);
+console.log(h3[2]); */
+
+//3. Après avoir récupéré tous les paragraphe, stock le second paragraphe et affiche dans un console.log
+/* 
+let paragraphe = document.getElementsByTagName('p')[1]
+console.log(paragraphe); */
+
+// 4. Après avoir récupéré tous les li, stock le 3eme li et affiche le dans un console.log
+
+/* let li = document.getElementsByTagName('li')[2]
+
+console.log(li); */

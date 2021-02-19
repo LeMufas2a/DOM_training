8e8231598b6776cd742709730935778ce2598421
LeMufas2a <codage.mufasa@gmail.com>
2021-02-19 22:28:31 +0100

    Exo_05

---
+++ recuperation_elements_part2.1/main.js
@@ -0,0 +1,7 @@
+console.log(document.getElementsByClassName('tartine')[0].innerText);
+
+let toast = document.getElementById("toast").innerText;
+console.log(toast);
+
+let tartine = document.getElementsByClassName("tartine")[1];
+console.log(tartine);

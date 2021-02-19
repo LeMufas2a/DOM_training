fb6533f518a9649da3608c3442c75f5946e64088
LeMufas2a <codage.mufasa@gmail.com>
2021-02-19 22:29:10 +0100

    Exo_08

---
+++ recuperation_elements_part2.4/main.js
@@ -0,0 +1,21 @@
+// 1 
+
+/* let redP = document.getElementsByClassName('redPurple');
+
+console.log(redP); */
+
+// 2
+
+console.log(document.querySelectorAll('redPurple'));
+
+// 3 
+
+let redPurple = document.querySelectorAll('redPurple');
+console.log(redPurple);
+// 4
+
+
+// 5 
+
+
+// 6

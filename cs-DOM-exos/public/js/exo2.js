7d0e6b355006993397c9a80f7ba9ac09957ecadb
LeMufas2a <codage.mufasa@gmail.com>
2021-02-25 23:19:12 +0100

    Exo_15&16

--- cs-DOM-exos/public/js/exo2.js
+++ cs-DOM-exos/public/js/exo2.js
@@ -35,7 +35,42 @@ container.addEventListener('click', (e) => {
 // 3 
 
 let ul = document.querySelector('ul')
-let li = document.querySelector('li')
+let li = document.createElement('li')
+let liAll = document.querySelectorAll('#exo-add-li > ul > li')[1]
 
 li.textContent = 'Hello World'
+
+ul.insertBefore(li, liAll)
+
+// 4
+
+let button = document.querySelector("#exo-li-from-input button");
+let myUl = document.querySelector("#exo-li-from-input>ul");
+let inputTodo = document.querySelector(".form-control");
+
+button.addEventListener("click", () => {
+    if (inputTodo.value.trim() != "") {
+        let myLi = document.createElement("li");
+        myLi.textContent = inputTodo.value;
+        myUl.appendChild(myLi);
+        inputTodo.value = "";
+    }
+});
+
+let editOrder = document.querySelectorAll("button")[2];
+editOrder.addEventListener("click", () => {
+    let liTodo = [...document.querySelectorAll("#exo-li-from-input>ul li")];
+    let newTodo = [];
+    for (let i = 0; i < liTodo.length; i) {
+        let random = Math.floor(Math.random() * liTodo.length);
+        newTodo.push(liTodo[random]);
+        liTodo.splice(random, 1);
+        document.querySelector("#exo-li-from-input>ul li").remove();
+    }
+    newTodo.forEach(e => {
+        let li = document.createElement("li");
+        li.textContent = e.textContent;
+        myUl.appendChild(li);
+    });
+});
 
